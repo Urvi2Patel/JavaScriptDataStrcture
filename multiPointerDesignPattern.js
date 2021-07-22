edf96a7640e2ff7301c8118d0a254b8e7988f242
Urvi2Patel <urvipatelon29@gmail.com>
2021-07-22 11:03:21 -0500

    Update multiPointerDesignPattern.js

--- multiPointerDesignPattern.js
+++ multiPointerDesignPattern.js
@@ -1,4 +1,11 @@
-function SumZero(arr) {
+// Write a function called sumZero which accepts sorted array of integers. 
+// The function should find first pair where the sum is zero. Return an array 
+// where that includes both values that sum to zero or undefined if pair does not 
+// exist.
+
+
+
+function sumZero(arr) {
     let left = 0;let sum;
     let right = arr.length - 1;
     while(left < right) {
